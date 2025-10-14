2ca928a9f3b75e3a654ec034193c52df919174a7
Eder Medeiros <140189508+edermedeiroos@users.noreply.github.com>
2025-10-14 19:49:24 -0300

    Exercicio01

--- ED1/Lista04/Exercicio01.cpp
+++ ED1/Lista04/Exercicio01.cpp
@@ -29,11 +29,25 @@ bool inserirElemento(LISTA* l, TIPOCHAVE chave, int posicao) {
     return true;
 }
 
-bool removerElement(LISTA* l, int posicao) {
-    if ((l->nElem = 0) || (posicao >= MAX))
-        return false;
+int buscaSequencial(LISTA* l, TIPOCHAVE ch) {
+		int i = 0;
+		
+		while (i < l->nElem){
+				if(ch == l->A[i].chave) return i;
+				else i++;
+		}
+		
+		return -1;
+}
 
-    l->A[posicao].chave = NULL;
+bool removerElemento(LISTA* l, TIPOCHAVE ch) {
+    int pos = buscaSequencial(l, ch);
+    if (pos == -1) return false;
+
+    for (int i = pos; i < l->nElem -1; i++) {
+        l->A[i] = l->A[i+1];
+    }
+
     l->nElem --;
     return true;
 }
@@ -43,5 +57,11 @@ int tamanhoLista(LISTA* l) {
 }
 
 void exibirElemento(LISTA* l) {
-    for 
+    for (int i=0; i<l->nElem; i++) {
+        cout << l->A[i].chave << endl;
+    }
+}
+
+bool reinicializarLista(LISTA* l) {
+    inicilizarLista(l);
 }
