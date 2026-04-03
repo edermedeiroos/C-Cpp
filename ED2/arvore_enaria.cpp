4ac884ac78e9dd4355000d306b6e829b8072cb53
Eder Medeiros <ederlissemedeiros@gmail.com>
2026-04-03 14:12:56 -0300

    DEF main()

--- ED2/arvore_enaria.cpp
+++ ED2/arvore_enaria.cpp
@@ -52,7 +52,7 @@ PONT_PESSOA buscaPessoa(PONT_PESSOA pessoa, PONT_PESSOA raiz){
     return(nullptr);
 }
 
-bool inserePessoa(string tipo_pessoa, PONT_PESSOA raiz, PONT_PESSOA pessoa, PONT_PESSOA pessoa_filho) {
+bool inserePessoa(string tipo_pessoa, PONT_PESSOA pessoa, PONT_PESSOA pessoa_filho) {
     if (!pessoa_filho || !pessoa) return(false);
 
     if (tipo_pessoa == "irmao") {
@@ -65,6 +65,9 @@ bool inserePessoa(string tipo_pessoa, PONT_PESSOA raiz, PONT_PESSOA pessoa, PONT
             }
             atual->prox_irmao = pessoa;
         }
+
+        pessoa->pai = pessoa_filho->pai;
+        pessoa->mae = pessoa_filho->mae;
         return(true);
     }
     else if (tipo_pessoa == "pai" && !pessoa_filho->pai) {
@@ -79,21 +82,50 @@ bool inserePessoa(string tipo_pessoa, PONT_PESSOA raiz, PONT_PESSOA pessoa, PONT
     return(false);
 }
 
-void exibirArvore(PONT_PESSOA raiz){
-    if (raiz == nullptr) return;
+void exibirArvore(PONT_PESSOA raiz) {
+    if (!raiz) return;
     
-    cout << raiz->nome << " " << raiz->sobre_nome << "(";
-    PONT_PESSOA irmao = raiz->prox_irmao;
-    PONT_PESSOA pai = raiz->pai;
-    PONT_PESSOA mae = raiz->mae;
+    cout << raiz->nome << " " << raiz->sobre_nome;
     
-    exibirArvore(irmao);
-    exibirArvore(pai);
-    exibirArvore(mae);
+    if (raiz->pai || raiz->mae) {
+        cout << " (";
+        if (raiz->pai) exibirArvore(raiz->pai);
+        if (raiz->pai && raiz->mae) cout << ", ";
+        if (raiz->mae) exibirArvore(raiz->mae);
+        cout << ")";
+    }
 
-    cout << ")";
+    if (raiz->prox_irmao) {
+        cout << " | Irmão(s): ";
+        exibirArvore(raiz->prox_irmao);
+    };
 }
 
 int main() {
-    PONT_PESSOA root = inicializaArvore("Eder", "Medeiros", "26/12/2007");
+    PONT_PESSOA eu = inicializaArvore("Eder", "Medeiros", "26/12/2007");
+    PONT_PESSOA irmao1 = criaPessoa("Dara", "Medeiros", "26/02/1997");
+    PONT_PESSOA mae = criaPessoa("Marilda", "Silva", "02/06/1966");
+    PONT_PESSOA pai = criaPessoa("Marcelino", "Medeiros", "08/05/1963");
+    PONT_PESSOA tio1 = criaPessoa("Mara", "Silva", "26/02/1987");
+    PONT_PESSOA tio2 = criaPessoa("Marcia", "Silva", "26/02/1964");
+    PONT_PESSOA tio3 = criaPessoa("Sandra", "Medeiros", "26/02/1997");
+    PONT_PESSOA tio4 = criaPessoa("Marcio", "Medeiros", "26/02/1997");
+    PONT_PESSOA avo1 = criaPessoa("Nair", "Silva", "26/02/1997");
+    PONT_PESSOA avo2 = criaPessoa("Antonio", "Silva", "26/02/1997");
+    PONT_PESSOA avo3 = criaPessoa("Marlene", "Medeiros", "26/02/1997");
+    PONT_PESSOA avo4 = criaPessoa("Marcelino", "Medeiros", "26/02/1997");
+
+    inserePessoa("irmao", irmao1, eu);
+    inserePessoa("mae", irmao1, eu);
+    inserePessoa("pai", irmao1, eu);
+    inserePessoa("irmao", tio1, mae);
+    inserePessoa("irmao", tio2, mae);
+    inserePessoa("irmao", tio3, pai);
+    inserePessoa("irmao", tio4, pai);
+    inserePessoa("mae", avo1, mae);
+    inserePessoa("pai", avo2, mae);
+    inserePessoa("mae", avo3, pai);
+    inserePessoa("pai", avo4, pai);
+
+    exibirArvore(eu);
 }
